62f392974cc3ca58b3f9825ae2d38b428d6aed6f
sid-js <sidhsreejil@gmail.com>
2023-02-22 22:03:59 +0530

    Added Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,26 @@
-import '../styles/globals.css'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
-import {Urbanist} from '@next/font/google'
+import "../styles/globals.css";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { Urbanist } from "@next/font/google";
+import Head from "next/head";
 
-const urbanist = Urbanist({display:'swap',variable:'--font-urbanist',subsets:['latin']})
-
+const urbanist = Urbanist({
+  display: "swap",
+  variable: "--font-urbanist",
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Component {...pageProps}  />
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
     </SessionContextProvider>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
